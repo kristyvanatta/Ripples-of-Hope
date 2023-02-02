2b0141f75ecada6d67ef5742ef9a07bf4d60fb5d
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-01 20:41:37 -0500

    Added some imports to the stories.js and changed some naming to fit what other project members used forpathing

--- client/src/pages/stories.js
+++ client/src/pages/stories.js
@@ -1,17 +1,19 @@
 import React from "react";
+import {client} from '../Ripples-of-Hope/client';
+import {queries} from 'queries.js';
 
 export default function stories() {
     const{id} = useParams();
-    const{loading, error, data} = useQuery({variables:{id}});
+    const{error, data} = client(queries, {variables:{id}});
     
     if (error) return <p>There was an error loading the page. Please try again. If there is still a problem please contacts us.</p>;
 
     return <>
         {!error && (
             <div>
-                <h1>{data.charity.name}</h1>
-                <p>{data.description}</p>
-                <img/>
+                <h1>{storyData.title}</h1>
+                <p>{storyData.description}</p>
+                <div>{storyData.image}</div>
             </div>
         )}
     </>;
