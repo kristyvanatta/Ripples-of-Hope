fd91fb1188e827a04c6a8967ba1f9bd2208ea35f
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-01 21:25:00 -0500

    Did a pull from main

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ const client = new ApolloClient ({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
-import stories from './pages/stories.js';
 
 function App() {
   return (
@@ -40,25 +39,6 @@ function App() {
       </Router>
     </ApolloProvider>
     
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Routes>
-          <Route path='/stories/:id' element={<stories />} />
-        </Routes>
-      </header>
-    </div>
   );
 }
 
