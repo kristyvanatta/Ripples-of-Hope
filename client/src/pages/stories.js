083880d23badbd40dfb558f0d0b4b88096d56697
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-01 20:22:37 -0500

    removed the loading code. They dont need a spinner.

--- client/src/pages/stories.js
+++ client/src/pages/stories.js
@@ -1,12 +1,13 @@
+import React from "react";
+
 export default function stories() {
     const{id} = useParams();
     const{loading, error, data} = useQuery({variables:{id}});
-
-    if (loading) return <Spinner/>;
+    
     if (error) return <p>There was an error loading the page. Please try again. If there is still a problem please contacts us.</p>;
 
     return <>
-        {!loading && !error && (
+        {!error && (
             <div>
                 <h1>{data.charity.name}</h1>
                 <p>{data.description}</p>
