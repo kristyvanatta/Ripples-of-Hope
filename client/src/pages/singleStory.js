d11cef61aa6ab50971319f77c12f33255f8cd632
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-02 22:52:09 -0500

    added a donate modal pop up button to singleStory.js. Also changed the button for Full Story on stories.js

--- client/src/pages/singleStory.js
+++ client/src/pages/singleStory.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from '@apollo/client';
 import { QUERY_STORY } from '../utils/queries.js';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const styles = {
   card: {
@@ -23,29 +26,76 @@ const styles = {
 
 function singleStory() {
 
-    const { loading, data } = useQuery(QUERY_STORY);
-  
-    const story = data?.singleStory || [];
-  
-  
-    return (
-      <>
-        <div className="container">
-          <h1>All Stories</h1>
-          {
-            loading ? (
-              <div>loading</div>
-            ) : (
-              story.map((story) => (<div style = {styles.card} key={story._id}>
-                <h1 style={styles.title}>{story.title}</h1>
-                <p style={styles.description}>{story.description}</p>
-                <div>
-                <img src={story.image} alt="ripples-of-hope" width="800" height="500"></img>
-                </div></div>))
-            )
-          }
-        </div>
-      </>
-    )
-  }
-  export default singleStory;
+  const { loading, data } = useQuery(QUERY_STORY);
+
+  const story = data?.story || [];
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Full Story</h1>
+        {
+          loading ? (
+            <div>loading</div>
+          ) : (
+            story.map((story) => (<div style = {styles.card} key={story._id}>
+              <h1 style={styles.title}>{story.title}</h1>
+              <p style={styles.description}>{story.description}</p>
+              <div>
+              <img src={story.image} alt="ripples-of-hope" width="800" height="500"></img>
+
+              <Button variant='primary' onClick={handleShow}>Donate</Button>
+              
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>We Appreciate Your Kindness!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                    <Form.Group className='mb-3' controlId='creditCard#'>
+                      <Form.Label>Credit Or Debit Card Number</Form.Label>
+                      <Form.Control
+                        placeholder='Card Number'
+                        autoFocus
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='name'>
+                      <Form.Label>Full Name On Card</Form.Label>
+                      <Form.Control
+                        placeholder='Full Name On Card'
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='expDate'>
+                      <Form.Label>Expiration Date</Form.Label>
+                      <Form.Control
+                        placeholder='Expiration Date'
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='cvv'>
+                      <Form.Label>CVV Number</Form.Label>
+                      <Form.Control
+                        placeholder='CVV Number'
+                      />
+                    </Form.Group>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant='secondary' onClick={handleClose}>Close</Button>
+                  <Button variant='primary' onClick={handleClose}>Submit</Button>
+                </Modal.Footer>
+              </Modal>
+              </div></div>))
+          )
+        }
+      </div>
+    </>
+  )
+}
+export default singleStory;
