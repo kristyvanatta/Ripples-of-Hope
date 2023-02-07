ded6a1c83ae19640056a50c29f51ddd57df17e3b
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-06 20:19:40 -0500

    forgot to save a some edits my last push

--- client/src/pages/stories.js
+++ client/src/pages/stories.js
@@ -1,7 +1,9 @@
-import React from "react";
-// import { useParams } from 'react-router-dom'
+import React, { useState } from "react";
 import { useQuery } from '@apollo/client';
 import { QUERY_STORIES } from '../utils/queries.js';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const styles = {
   card: {
@@ -13,6 +15,7 @@ const styles = {
     minHeight: 50,
     lineHeight: 3.5,
     fontSize: '1.2rem',
+    textAlign: "center",
     color: 'black',
     padding: '0 20px',
   },
@@ -27,11 +30,14 @@ function Stories() {
 
   const allStories = data?.stories || [];
 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
       <div className="container">
-        <h2>All Stories</h2>
+        <h1>All Stories</h1>
         {
           loading ? (
             <div>loading</div>
@@ -40,7 +46,53 @@ function Stories() {
               <h1 style={styles.title}>{story.title}</h1>
               <p style={styles.description}>{story.description}</p>
               <div>
-              <img src={story.image} alt="ripples-of-hope" width="100%" height="500"></img>
+              <img src={story.image} alt="ripples-of-hope" width="800" height="500"></img>
+              
+              <Button variant='primary' onClick="self.location='../pages/singleStory.js'">Full Story</Button>
+
+              <Button variant='primary' onClick={handleShow}>Donate</Button>
+              
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>We Appreciate Your Kindness!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                    <Form.Group className='mb-3' controlId='creditCard#'>
+                      <Form.Label>Credit Or Debit Card Number</Form.Label>
+                      <Form.Control
+                        placeholder='Card Number'
+                        autoFocus
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='name'>
+                      <Form.Label>Full Name On Card</Form.Label>
+                      <Form.Control
+                        placeholder='Full Name On Card'
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='expDate'>
+                      <Form.Label>Expiration Date</Form.Label>
+                      <Form.Control
+                        placeholder='Expiration Date'
+                      />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='cvv'>
+                      <Form.Label>CVV Number</Form.Label>
+                      <Form.Control
+                        placeholder='CVV Number'
+                      />
+                    </Form.Group>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant='secondary' onClick={handleClose}>Close</Button>
+                  <Button variant='primary' onClick={handleClose}>Submit</Button>
+                </Modal.Footer>
+              </Modal>
               </div></div>))
           )
         }
