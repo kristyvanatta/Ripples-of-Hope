2b9f85498ac61eba2891f38631ca87be5ebf4db1
Kristy Vanatta <kristylvanatta@gmail.com>
2023-02-06 21:04:15 -0500

    path error fix for deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 
 const { typeDefs, resolvers } = require("./schema");
 const db = require("./config/connection");
